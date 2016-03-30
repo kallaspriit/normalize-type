433b75e41b7cc83f574abf327dd3ab07ec7f3ccd
Priit Kallas <kallaspriit@gmail.com>
2016-03-30 14:52:24 +0300

    chore(tests): Improved tests.
    Added test for converting numberic integer strings.

--- test/index.test.js
+++ test/index.test.js
@@ -7,4 +7,8 @@ describe('normalize-type', () => {
 		expect(normalizeType(2, 5)).to.equal(7);
 	});
 
+	it('should convert numeric integer strings to numbers', () => {
+		expect(normalizeType(3, 1)).to.equal(4);
+	});
+
 });
