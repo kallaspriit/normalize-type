991171306d63c5dac3efb811805147a9cca01718
Priit Kallas <kallaspriit@gmail.com>
2018-02-07 19:28:30 +0200

    Improved numbers handling.

--- src/index.ts
+++ src/index.ts
@@ -1,59 +1,59 @@
 interface IndexableObject {
-	[x: string]: any;
+  [x: string]: any;
 }
 
 export function normalizeType(arg: any[]): any[];
 export function normalizeType(arg: any): any;
 export function normalizeType<T>(arg: any): T;
 export function normalizeType(arg: any): any {
-	// handle arrays
-	if (Array.isArray(arg)) {
-		return arg.map(item => normalizeType(item));
-	}
+  // handle arrays
+  if (Array.isArray(arg)) {
+    return arg.map(normalizeType);
+  }
 
-	// handle ordinary objects (maps) but not dates etc
-	if (typeof arg === 'object' && arg !== null && arg.constructor === Object) {
-		return Object.keys(arg).reduce((obj: IndexableObject, key) => {
-			obj[key] = normalizeType(arg[key]);
+  // handle ordinary objects (maps) but not dates etc
+  if (typeof arg === "object" && arg !== null && arg.constructor === Object) {
+    return Object.keys(arg).reduce<{}>((obj: IndexableObject, key) => {
+      obj[key] = normalizeType(arg[key]);
 
-			return obj;
-		}, {});
-	}
+      return obj;
+    }, {});
+  }
 
-	// do not modify non-strings from this point on
-	if (typeof arg !== 'string') {
-		return arg;
-	}
+  // do not modify non-strings from this point on
+  if (typeof arg !== "string") {
+    return arg;
+  }
 
-	// handle special empty string case that can be seen as a number by isNaN
-	if (arg.length === 0) {
-		return arg;
-	}
+  // handle special empty string case that can be seen as a number by isNaN
+  if (arg.length === 0) {
+    return arg;
+  }
 
-	// map of direct string conversions
-	const conversionMap: IndexableObject = {
-		true: true,
-		false: false,
-	};
-	const conversionValue = conversionMap[arg];
+  // map of direct string conversions
+  const conversionMap: IndexableObject = {
+    true: true,
+    false: false,
+  };
+  const conversionValue = conversionMap[arg];
 
-	// return the conversion map value if matched
-	if (typeof conversionValue !== 'undefined') {
-		return conversionValue;
-	}
+  // return the conversion map value if matched
+  if (typeof conversionValue !== "undefined") {
+    return conversionValue;
+  }
 
-	// handle undefined special case
-	if (arg === 'undefined') {
-		return undefined;
-	}
+  // handle undefined special case
+  if (arg === "undefined") {
+    return undefined;
+  }
 
-	// handle numeric values
-	if (!isNaN(Number(arg))) {
-		return Number(arg);
-	}
+  // handle numeric values
+  if (!isNaN(Number(arg)) && /^-?[0-9]{1,}(\.[0-9]+)?$/.test(arg)) {
+    return Number(arg);
+  }
 
-	// return the string as is
-	return arg;
+  // return the string as is
+  return arg;
 }
 
 export default normalizeType;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,101 +1,117 @@
-import 'ts-jest';
-import normalizeType from '../src/index';
+import "ts-jest";
+import normalizeType from "../src/index";
 
-describe('normalize-type', () => {
-	it('should leave strings to be strings', () => {
-		expect(normalizeType('foo')).toEqual('foo');
-		expect(normalizeType('')).toEqual('');
-	});
+describe("normalize-type", () => {
+  it("should leave strings to be strings", () => {
+    expect(normalizeType("foo")).toEqual("foo");
+    expect(normalizeType("")).toEqual("");
+  });
 
-	it('should not modify non-strings', () => {
-		const nonStringValues = [123, 1, 0, 1.0, 0.0, new Date(), undefined, null, {}, [], {a: 1}, [1, 2, 3]];
+  it("should not modify non-strings", () => {
+    const nonStringValues = [123, 1, 0, 1.0, 0.0, new Date(), undefined, null, {}, [], { a: 1 }, [1, 2, 3]];
 
-		nonStringValues.forEach(value => {
-			expect(normalizeType(value)).toEqual(value);
-		});
-	});
+    nonStringValues.forEach(value => {
+      expect(normalizeType(value)).toEqual(value);
+    });
+  });
 
-	it('should convert numeric integer strings to numbers', () => {
-		expect(normalizeType('123')).toEqual(123);
-		expect(normalizeType('1')).toEqual(1);
-		expect(normalizeType('0')).toEqual(0);
-	});
+  it("should convert numeric integer strings to numbers", () => {
+    expect(normalizeType("123")).toEqual(123);
+    expect(normalizeType("1")).toEqual(1);
+    expect(normalizeType("0")).toEqual(0);
+  });
 
-	it('should convert numeric float strings to numbers', () => {
-		expect(normalizeType('123.4')).toEqual(123.4);
-		expect(normalizeType('1.0')).toEqual(1.0);
-		expect(normalizeType('0.0')).toEqual(0.0);
-		expect(normalizeType('.12')).toEqual(0.12);
-	});
+  it("should convert numeric float strings to numbers", () => {
+    expect(normalizeType("123.4")).toEqual(123.4);
+    expect(normalizeType("1.0")).toEqual(1.0);
+    expect(normalizeType("0.0")).toEqual(0.0);
+  });
 
-	it('should preserve number-like invalid number values', () => {
-		const numberLikeStrings = ['1..0', '..0', '10p'];
+  it("should convert negative numeric strings to numbers", () => {
+    expect(normalizeType("-123")).toEqual(-123);
+    expect(normalizeType("-1")).toEqual(-1);
+    expect(normalizeType("-0")).toEqual(-0);
+    expect(normalizeType("-123.4")).toEqual(-123.4);
+    expect(normalizeType("-1.0")).toEqual(-1.0);
+    expect(normalizeType("-0.0")).toEqual(-0.0);
+  });
 
-		numberLikeStrings.forEach(value => {
-			expect(normalizeType(value)).toEqual(value);
-		});
-	});
+  it("should not convert strings that can look like special numbers", () => {
+    expect(normalizeType("+37255412345")).toEqual("+37255412345");
+    expect(normalizeType(".12")).toEqual(".12");
+    expect(normalizeType("+1")).toEqual("+1");
+    expect(normalizeType("+")).toEqual("+");
+    expect(normalizeType("-")).toEqual("-");
+  });
 
-	it('should convert "true" and "false" to booleans', () => {
-		expect(normalizeType('true')).toEqual(true);
-		expect(normalizeType('false')).toEqual(false);
-	});
+  it("should preserve number-like invalid number values", () => {
+    const numberLikeStrings = ["1..0", "..0", "10p"];
 
-	it('should convert "undefined" to undefined', () => {
-		expect(normalizeType('undefined')).toEqual(undefined);
-	});
+    numberLikeStrings.forEach(value => {
+      expect(normalizeType(value)).toEqual(value);
+    });
+  });
 
-	it('should not convert non-lowercase special mappings', () => {
-		const nonLowercaseConversions = ['TRUE', 'FALSE', 'True', 'False'];
+  it('should convert "true" and "false" to booleans', () => {
+    expect(normalizeType("true")).toEqual(true);
+    expect(normalizeType("false")).toEqual(false);
+  });
 
-		nonLowercaseConversions.forEach(value => {
-			expect(normalizeType(value)).toEqual(value);
-		});
-	});
+  it('should convert "undefined" to undefined', () => {
+    expect(normalizeType("undefined")).toEqual(undefined);
+  });
 
-	it('should convert arrays of values', () => {
-		expect(normalizeType([])).toEqual([]);
-		expect(normalizeType(['123'])).toEqual([123]);
-		expect(normalizeType(['0'])).toEqual([0]);
-		expect(normalizeType(['1'])).toEqual([1]);
-		expect(normalizeType(['test', '1', '2.3'])).toEqual(['test', 1, 2.3]);
-	});
+  it("should not convert non-lowercase special mappings", () => {
+    const nonLowercaseConversions = ["TRUE", "FALSE", "True", "False"];
 
-	it('should convert nested arrays of values', () => {
-		expect(normalizeType([[]])).toEqual([[]]);
-		expect(normalizeType([['123']])).toEqual([[123]]);
-		expect(normalizeType([['0']])).toEqual([[0]]);
-		expect(normalizeType([['1']])).toEqual([[1]]);
-		expect(normalizeType([['test', '1', '2.3']])).toEqual([['test', 1, 2.3]]);
-		expect(normalizeType([['123', '234'], ['5.5']])).toEqual([[123, 234], [5.5]]);
-	});
+    nonLowercaseConversions.forEach(value => {
+      expect(normalizeType(value)).toEqual(value);
+    });
+  });
 
-	it('should convert object maps', () => {
-		expect(normalizeType({})).toEqual({});
-		expect(normalizeType({a: 'test'})).toEqual({a: 'test'});
-		expect(normalizeType({a: '123'})).toEqual({a: 123});
-	});
+  it("should convert arrays of values", () => {
+    expect(normalizeType([])).toEqual([]);
+    expect(normalizeType(["123"])).toEqual([123]);
+    expect(normalizeType(["0"])).toEqual([0]);
+    expect(normalizeType(["1"])).toEqual([1]);
+    expect(normalizeType(["test", "1", "2.3"])).toEqual(["test", 1, 2.3]);
+  });
 
-	it('should convert nested object maps', () => {
-		expect(normalizeType({a: {b: 'test'}})).toEqual({a: {b: 'test'}});
-		expect(normalizeType({a: {b: '123'}})).toEqual({a: {b: 123}});
-	});
+  it("should convert nested arrays of values", () => {
+    expect(normalizeType([[]])).toEqual([[]]);
+    expect(normalizeType([["123"]])).toEqual([[123]]);
+    expect(normalizeType([["0"]])).toEqual([[0]]);
+    expect(normalizeType([["1"]])).toEqual([[1]]);
+    expect(normalizeType([["test", "1", "2.3"]])).toEqual([["test", 1, 2.3]]);
+    expect(normalizeType([["123", "234"], ["5.5"]])).toEqual([[123, 234], [5.5]]);
+  });
 
-	it('should handle mixed content', () => {
-		expect(
-			normalizeType({
-				id: '2',
-				name: 'Jack Daniels',
-				age: '26',
-				height: '1.84',
-				accessLevels: ['1', '3'],
-			}),
-		).toEqual({
-			id: 2,
-			name: 'Jack Daniels',
-			age: 26,
-			height: 1.84,
-			accessLevels: [1, 3],
-		});
-	});
+  it("should convert object maps", () => {
+    expect(normalizeType({})).toEqual({});
+    expect(normalizeType({ a: "test" })).toEqual({ a: "test" });
+    expect(normalizeType({ a: "123" })).toEqual({ a: 123 });
+  });
+
+  it("should convert nested object maps", () => {
+    expect(normalizeType({ a: { b: "test" } })).toEqual({ a: { b: "test" } });
+    expect(normalizeType({ a: { b: "123" } })).toEqual({ a: { b: 123 } });
+  });
+
+  it("should handle mixed content", () => {
+    expect(
+      normalizeType({
+        id: "2",
+        name: "Jack Daniels",
+        age: "26",
+        height: "1.84",
+        accessLevels: ["1", "3"],
+      }),
+    ).toEqual({
+      id: 2,
+      name: "Jack Daniels",
+      age: 26,
+      height: 1.84,
+      accessLevels: [1, 3],
+    });
+  });
 });
